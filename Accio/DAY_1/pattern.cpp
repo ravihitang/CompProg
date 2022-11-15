9b582e56da62641abbdc496c3f650c3dbbacd04c
ravihitang <ravihitangofficial25@gmail.com>
2022-11-16 01:20:43 +0530

    pattern solved

---
+++ Accio/DAY_1/pattern.cpp
@@ -0,0 +1,18 @@
+//Print Pattern :
+//   * * * * 
+//   * * *
+//   * *
+//   *
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+        for(int j=0;j<4;j++){
+            for(int i=j;i<4;i++)
+            {
+             cout<<"*";
+             cout<<" ";
+            }
+            cout<<"\n";
+        }
+    return 0;
+}
