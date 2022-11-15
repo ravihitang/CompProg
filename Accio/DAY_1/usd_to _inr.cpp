2ed8b7450c5a3b5573cb73217fbf5c2d863f4669
ravihitang <ravihitangofficial25@gmail.com>
2022-11-16 01:22:58 +0530

    currency conversion solved

---
+++ Accio/DAY_1/usd_to _inr.cpp
@@ -0,0 +1,11 @@
+// Given a US dollar convert the USD to INR (Indian Rupee)
+//Conversion rate : 1 USD = 80 RS
+
+# include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int usd;
+    cin>>usd;
+    cout<<usd*80;
+    return 0;
+}
